2f69436356acd4f1ff55b9d961204ac24957e4c1
Vgnttv <o.vignani@gmail.com>
2019-06-05 14:35:37 +0200

    Implemented login and signup actions and reducers, created contants file, jwt file and middleware file

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,14 @@
 import { combineReducers } from "redux";
 import ads from "./ads";
 import ad from "./ad";
+import users from "./users";
+import login from "./login";
+import signup from "./signup";
 
 export default combineReducers({
   ads,
-  ad
+  ad,
+  login,
+  signup,
+  users
 });
